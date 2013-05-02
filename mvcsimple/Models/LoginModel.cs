410f53eb44bc54bcd24f1c20884d1b24bae1d7d1
Cassim Vanker <cassimv@pcwatch.cc>
2013-05-02 07:55:21 +0200

    5th Add

---
+++ mvcsimple/Models/LoginModel.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MvcSimple.Models
+{
+    public class LoginModel
+    {
+        [Required(ErrorMessage = "Enter your username")]
+        [Display(Name = "User name")]
+        public string UserName { get; set; }
+
+        [Required(ErrorMessage = "Enter your password")]
+        [DataType(DataType.Password)]        
+        [Display(Name = "Password")]    
+        public string Password { get; set; }
+
+        [Display(Name = "Remember me?")]
+        public bool RememberMe { get; set; }
+
+    }
+}
---
+++ mvcsimple/controllers/LoginController.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MvcSimple.Models;
+using WebMatrix.WebData;
+
+namespace MvcSimple.Controllers
+{
+    public class LoginController : Controller
+    {
+        //
+        // GET: /Login/
+        [AllowAnonymous]
+        [HttpGet]
+        public ActionResult Index()
+        {
+            Models.LoginModel objlm = new LoginModel();
+            return View(objlm);
+        }
+
+        [AllowAnonymous]
+        [ValidateAntiForgeryToken]
+        [HttpPost]
+        public ActionResult Index(LoginModel model, string returnUrl)
+        {
+            if (ModelState.IsValid && WebSecurity.Login(model.UserName, model.Password, persistCookie: model.RememberMe))
+            {
+                return RedirectToLocal(returnUrl);
+            }
+
+            // If we got this far, something failed, redisplay form
+            ModelState.AddModelError("", "The user name or password provided is incorrect.");
+            return View(model);
+        }
+
+
+        private ActionResult RedirectToLocal(string returnUrl)
+        {
+            if (Url.IsLocalUrl(returnUrl))
+            {
+                return Redirect(returnUrl);
+            }
+            else
+            {
+                return RedirectToAction("Default","Index");
+            }
+        }
+
+       [AllowAnonymous]
+        public ActionResult Logout()
+        {
+            WebSecurity.Logout();
+           return RedirectToAction("Index", "Default");
+        }
+    }
+}
